a1d22b8c61f922c84cdef4d620dd4aac9c82dafa
drewvillani <dvillani1@student.monroecc.edu>
2025-01-26 00:03:12 -0500

    Add files via upload

--- buggy_script.m
+++ buggy_script.m
@@ -1,11 +1,3 @@
 radius = 5;
-area = pi * (radius^2);
-disp('The area of the circle is: ')
-disp(area)
-% The area of the circle is: 
-%   78.5398
-
-% or
-
-fprintf("The area of the circle is: %.2f\n", area) % %.2f formats to 2 decimal places
-% The area of the circle is: 78.54
+area = pi * radious^2;
+disp('The area of the circle is: 'area) % Incorrect use of disp function
---
+++ fixed_script.m
@@ -0,0 +1,11 @@
+radius = 5;
+area = pi * (radius^2);
+disp('The area of the circle is: ')
+disp(area)
+% The area of the circle is: 
+%   78.5398
+
+% or
+
+fprintf("The area of the circle is: %.2f\n", area) % %.2f formats to 2 decimal places
+% The area of the circle is: 78.54
