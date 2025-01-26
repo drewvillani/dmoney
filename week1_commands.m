a445cb79f43e2bb672e74aff2e303d3fff0a94bf
drewvillani <dvillani1@student.monroecc.edu>
2025-01-25 22:19:02 -0500

    Add files via upload

---
+++ week1_commands.m
@@ -0,0 +1,22 @@
+a = 4;
+b = 3;
+
+result = a / b; % 4/3
+
+
+c = 5;
+d = 2;
+
+result2 = c*d; % 5(2)
+
+
+e = 7;
+f = 9;
+
+result3 = e^f; % 7 to the 9th power
+
+% clc - clears the command window
+% clear - clears our workspace tab, deleting variables we set
+% whos - displays the fine details of each variable set
+
+% - Drew Villani
